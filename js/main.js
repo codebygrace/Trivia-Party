e919e6f8e9f2cabc532e4944912c13138874300f
Grace Mills <gracemills.1023@gmail.com>
2022-04-22 12:50:15 -0500

    added function that accepts user input and compares it to fetch output

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
 const jeopardy = "https://jservice.io/api/random";
 const questionOutput = document.querySelector('.question');
 const newQuestion = document.querySelector('.new-q');
+const answerButton = document.querySelector('.answer-button');
+var answer = "";
 newQuestion.addEventListener('click', displayRandomQuestion);
+answerButton.addEventListener('click', getAnswer);
 
 function displayRandomQuestion(){
   fetch(jeopardy)
   .then(function(res){
     return res.json();
-  })
-  .then(function(data){
+  }).then(function(data){
     data.forEach(function(result){
       if(result.value === null || result.question === ""){
         displayRandomQuestion();
       } else {
-        questionOutput.innerHTML = `Category: "${result.category.title}" <br> $${result.value} <br> ${result.question}`;
+        questionOutput.innerHTML = `Category: "${result.category.title}"
+        <br>
+        $${result.value} 
+        <br>
+        ${result.question}`;
       }
+      console.log(result);
+      answer = result.answer;
     });
-  }).catch(function(error){
-   output.innerHTML += error;
   })
+}
+
+function getAnswer(){
+  console.log(answer);
+  console.log(document.querySelector('.answer-input').value);
+  if (document.querySelector('.answer-input').value === answer){
+    console.log("Correct");
+  } else {
+    console.log("Try Again");
+  }
 }
