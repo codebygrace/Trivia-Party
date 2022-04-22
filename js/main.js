24245e7f5257196f1c1dae022a64c97f51542663
Grace Mills <gracemills.1023@gmail.com>
2022-04-22 16:10:28 -0500

    added a feature that updates the user score after answer submit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const questionOutput = document.querySelector('.question');
 const newQuestion = document.querySelector('.new-q');
 const answerButton = document.querySelector('.answer-button');
 var answer = "";
+var value = "";
+var score = "";
+var input = document.querySelector('.answer-input').value;
 newQuestion.addEventListener('click', displayRandomQuestion);
 answerButton.addEventListener('click', getAnswer);
 
@@ -23,16 +26,28 @@ function displayRandomQuestion(){
       }
       console.log(result);
       answer = result.answer;
+      value = result.value;
     });
   })
 }
 
-function getAnswer(){
+function getAnswer(e, displayRandomQuestion){
+  e.preventDefault();
   console.log(answer);
-  console.log(document.querySelector('.answer-input').value);
-  if (document.querySelector('.answer-input').value === answer){
+  console.log(input);
+  if(document.querySelector('.answer-input').value == answer){
+    score = score + value;
+    questionOutput.innerHTML = `
+    Correct!
+    <br>
+    Score: $${score}`;
     console.log("Correct");
   } else {
-    console.log("Try Again");
+    score = score - value;
+    questionOutput.innerHTML = `
+    Incorrect :(
+    <br>
+    Score: $${score}`;
+    console.log("Incorrect");
   }
-}
+ }
